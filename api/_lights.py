4a1d95d92b3ca332891316befeb17ac488f16af9
Pratyoush Srivastava <pratyoush93@gmail.com>
2020-03-15 22:20:25 -0500

    Adds methods to change brightness, hue, saturation and toggle lights.

--- api/_lights.py
+++ api/_lights.py
@@ -11,9 +11,48 @@ class Lights:
         print(response.content)
         return response
 
-    def change_lights(self, username, id):
+    def get_light_status(self, username, id):
+        response = requests.get("http://{ip}/api/{username}/lights/{id}".format(ip = self.ip, username = username, id = id))
+        print(response.status_code)
+        print(response.content)
+        return response
+
+    def toggle_lights(self, username, id, toggleState):
         response = requests.put("http://{ip}/api/{username}/lights/{id}/state".format(ip = self.ip, username = username, id = id),
-        json = {"on":False})
+        json = {"on":toggleState})
+        print(response.status_code)
+        print(response.content)
+        return response
+
+    def change_brightness(self, username, id, brightness):
+        if brightness < 1:
+            brightness = 1
+        elif brightness > 254:
+            brightness = 254
+        response = requests.put("http://{ip}/api/{username}/lights/{id}/state".format(ip = self.ip, username = username, id = id),
+        json = {"bri":brightness})
+        print(response.status_code)
+        print(response.content)
+        return response
+
+    def change_hue(self, username, id, hue):
+        if hue < 0:
+            hue = 0
+        elif hue > 65535:
+            hue = 65535
+        response = requests.put("http://{ip}/api/{username}/lights/{id}/state".format(ip = self.ip, username = username, id = id),
+        json = {"hue":hue})
+        print(response.status_code)
+        print(response.content)
+        return response
+
+    def change_saturation(self, username, id, saturation):
+        if saturation < 0:
+            saturation = 0
+        elif saturation > 254:
+            saturation = 254
+        response = requests.put("http://{ip}/api/{username}/lights/{id}/state".format(ip = self.ip, username = username, id = id),
+        json = {"sat":saturation})
         print(response.status_code)
         print(response.content)
         return response
